8fe76fe3b26486789430fe5ede8dc14b605f6acc
nahimdhaney <nahimdhaney@hotmail.com>
2021-02-16 18:29:38 -0400

    all project.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <px-header></px-header>
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+    <!-- <px-assets-table></px-assets-table> -->
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
+import PxHeader from '@/components/PxHeader'
+// import PxAssetsTable from "@/components/PxAssetsTable"
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    PxHeader,
+    // PxAssetsTable
   }
 };
 </script>
---
+++ src/views/CoinDetail.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="flex-col">
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+    <template v-if="!isLoading">
+      <div class="flex flex-col sm:flex-row justify-around items-center">
+        <div class="flex flex-col items-center">
+          <img
+            :src="
+              `https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`
+            "
+            :alt="asset.name"
+            class="w-20 h-20 mr-5"
+          />
+          <h1 class="text-5xl">
+            {{ asset.name }}
+            <small class="sm:mr-2 text-gray-500">{{ asset.symbol }}</small>
+          </h1>
+        </div>
+
+        <div class="my-10 flex flex-col">
+          <ul>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Ranking</b>
+              <span>#{{ asset.rank }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio actual</b>
+              <span>{{ asset.priceUsd | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio más bajo</b>
+              <span>{{ min | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio más alto</b>
+              <span>{{ max | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio Promedio</b>
+              <span>{{ avg | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Variación 24hs</b>
+              <span>{{ asset.changePercent24Hr | percent }}</span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
+          <button
+            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          >Cambiar</button>
+
+          <div class="flex flex-row my-5">
+            <label class="w-full" for="convertValue">
+              <input
+                id="convertValue"
+                type="number"
+                class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+              />
+            </label>
+          </div>
+
+          <span class="text-xl"></span>
+        </div>
+      </div>
+      <!-- <line-chart id="users-chart" :data="{'2017-05-13': 2, '2017-05-14': 5}"></line-chart> -->
+      <!-- <pie-chart :data="[['Blueberry', 44], ['Strawberry', 23]]"></pie-chart> -->
+      <h3 class="text-xl my-10">Mejores Ofertas de Cambio</h3>
+      <table>
+        <tr v-for="m in markers" :key="`${m.exchangeId}-${m.priceUsd}`" class="border-b">
+          <td>
+            <b>{{ m.exchangeId }}</b>
+          </td>
+          <td>{{ m.priceUsd | dollar }}</td>
+          <td>{{ m.baseSymbol }} / {{ m.quoteSymbol }}</td>
+          <td>
+            <px-button
+              :is-loading="m.isLoading || false"
+              v-if="!m.url"
+              @custom-click="getWebSite(m)"
+            >
+              <slot>Obtener Link</slot>
+            </px-button>
+
+            <a v-else class="hover:underline text-green-600" target="_blanck">{{ m.url }}</a>
+          </td>
+        </tr>
+      </table>
+
+    </template>
+  </div>
+</template>
+
+<script>
+import PxButton from '@/components/PxButton'
+import api from '@/api'
+
+export default {
+  name: 'CoinDetail',
+  components:{PxButton},
+  data() {
+    return {
+      isLoading:false,
+      asset: {},
+      history: [],
+      markers:[]
+    }
+  },
+
+  computed: {
+    min() {
+      return Math.min(
+        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
+      )
+    },
+
+    max() {
+      return Math.max(
+        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
+      )
+    },
+
+    avg() {
+      return Math.abs(
+        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
+      )
+    },
+    // chartData() {
+    //   const data = []
+    //   this.history.map(h => {
+    //     data.push([h.date, parseFloat(h.priceUsd).toFixed(2)])
+    //   })
+    //   return data
+    // }
+  },
+
+  created() {
+    this.getCoin()
+  },
+
+  methods: {
+    getCoin() {
+      this.isLoading=true
+      const id = this.$route.params.id
+      Promise.all([api.getAsset(id), api.getAssetHistory(id),api.getMarkers(id)]).then(
+        ([asset, history,markers]) => {
+          this.asset = asset
+          this.history = history,
+          this.markers = markers
+        }
+      ).finally(()=>(this.isLoading=false))
+    },
+    getWebSite(exchange){
+      return api.getExchange(exchange.exchangeId).then(res => exchange.url = res.exchangeUrl )
+    }
+  }
+}
+</script>
+
+<style scoped>
+td {
+  padding: 10px;
+  text-align: center;
+}
+</style>
+
